a5de14e2222dc221f7a6318566a7db4d6647e5aa
Nick <nwestallen@gmail.com>
2024-01-07 14:08:31 -0800

    converts opendex to cljs, sprites update

--- src/main/app/components/monitor.cljs
+++ src/main/app/components/monitor.cljs
@@ -1,12 +1,10 @@
 (ns app.components.monitor
   (:require [helix.core :refer [defnc $]]
             [helix.hooks :as hooks]
-            [helix.dom :as d]))
+            [helix.dom :as d]
+            [app.components.opendex :refer [opendex]]))
 
 (defnc monitor [{:keys [sprite open?]}]
   (if open?
-  (d/div {:class-name "relative h-full"}
-   (d/img {:src "Open Pokedex.svg" :class-name "absolute max-w-full max-h-full"})
-    (d/div {:class-name "absolute bg-opacity-0 bg-lime-500 w-[27%] h-[32%] mt-[250px] ml-[10%] rounded-xl"}
-      (d/img {:src sprite :class-name "max-w-full max-h-full"})))
-  (d/img {:src "Closed Pokedex.svg"})))
+  (d/div {:class-name "relative h-full"} ($ opendex {:sprite sprite}))
+  (d/img {:src "ClosedPokedex.svg"})))
--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -7,13 +7,17 @@
 
 (defnc app []
   {:helix/features {:fast-refresh true}}
-  (let [[state set-state] (hooks/use-state {:open? false})]
+  (let [[state set-state] (hooks/use-state {:open? false :sprite "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/373.png"})]
     (d/div {:class-name "h-screen"}
      (d/h1 {:class-name "text-4xl font-bold p-5 bg-green-500"} "Pokedex")
       (d/div {:class-name "relative p-5 h-3/4"}
-        ($ monitor {:sprite "dragonite.png" :open? (:open? state)}))
+        ($ monitor {:sprite (:sprite state) :open? (:open? state)}))
       (d/div (d/button {:class-name "bg-blue-500 rounded-xl p-2 m-3"
-                        :on-click #(set-state assoc :open? (not (:open? state)))}"Open/Close")))))
+                        :on-click #(set-state assoc :open? (not (:open? state)))}"Open/Close")
+        (d/button {:class-name "bg-blue-500 rounded-xl p-2 m-3"
+                   :on-click #(set-state assoc :sprite "../dragonite.png")} "Change Sprite"))
+      )))
+
 
 ;; start your app with your favorite React renderer
 (defn ^:export init []
