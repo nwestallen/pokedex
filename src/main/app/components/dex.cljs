673b361ef28cd02569b86a93b20fe3239ffdbf69
Nick <nwestallen@gmail.com>
2024-01-08 23:19:54 -0800

    rename monitor to dex

--- src/main/app/components/dex.cljs
+++ src/main/app/components/dex.cljs
@@ -1,10 +1,10 @@
-(ns app.components.monitor
+(ns app.components.dex
   (:require [helix.core :refer [defnc $]]
             [helix.hooks :as hooks]
             [helix.dom :as d]
             [app.components.opendex :refer [opendex]]))
 
-(defnc monitor [{:keys [sprite open?]}]
+(defnc dex [{:keys [sprite open?]}]
   (if open?
   (d/div {:class-name "relative h-full"} ($ opendex {:sprite sprite}))
   (d/img {:src "ClosedPokedex.svg"})))
--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -3,7 +3,7 @@
             [helix.hooks :as hooks]
             [helix.dom :as d]
             ["react-dom/client" :as rdom]
-            [app.components.monitor :refer [monitor]]))
+            [app.components.dex :refer [dex]]))
 
 (defnc app []
   {:helix/features {:fast-refresh true}}
@@ -11,7 +11,7 @@
     (d/div {:class-name "h-screen"}
      (d/h1 {:class-name "text-4xl font-bold p-5 bg-green-500"} "Pokedex")
       (d/div {:class-name "relative p-5 h-3/4"}
-        ($ monitor {:sprite (:sprite state) :open? (:open? state)}))
+        ($ dex {:sprite (:sprite state) :open? (:open? state)}))
       (d/div (d/button {:class-name "bg-blue-500 rounded-xl p-2 m-3"
                         :on-click #(set-state assoc :open? (not (:open? state)))}"Open/Close")
         (d/button {:class-name "bg-blue-500 rounded-xl p-2 m-3"
